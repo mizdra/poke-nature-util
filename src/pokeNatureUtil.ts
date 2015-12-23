d541d97f8c233740383679ec22a7e508e93c21e5
mizdra <pp.mizdra@gmail.com>
2015-12-24 00:52:50 +0900

    pokeNatureUtil.tsを作成

---
+++ src/pokeNatureUtil.ts
@@ -0,0 +1,17 @@
+export default class PokeNatureUtil {
+    private static natures: string[] = [
+        "がんばりや", "さみしがり", "ゆうかん", "いじっぱり", "やんちゃ",
+        "ずぶとい", "すなお", "のんき", "わんぱく", "のうてんき",
+        "おくびょう", "せっかち", "まじめ", "ようき", "むじゃき",
+        "ひかえめ", "おっとり", "れいせい", "てれや", "うっかりや",
+        "おだやか", "おとなしい", "なまいき", "しんちょう", "きまぐれ"
+    ];
+
+    static toNumber(natureName: string): number {
+        return this.natures.indexOf(natureName);
+    }
+
+    static toString(natureNum: number): string {
+        return this.natures[natureNum];
+    }
+}
